52f56e3a98eae528a155dbed9911ccf2e940df98
Maxim <101204832+shp3x@users.noreply.github.com>
2024-08-13 14:45:11 +0300

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,85 @@
+const header = new Headers()
+
+function changeTheme() {
+    if (document.body.style.backgroundColor == 'rgb(30, 35, 55)') {
+        document.body.style.backgroundColor = 'rgb(247, 249, 251)'
+        document.getElementById('themeIcon').className = 'far fa-moon'
+        document.getElementById('themeIcon').style.color = 'rgb(0, 0, 0)'
+        document.getElementById('tonLogo').src = 'assets/img/ton_logo_light_background.svg'
+        document.getElementById('mainBannerText').style.color = 'rgb(0, 0, 0)'
+        document.getElementById('statsBanner').style.color = 'rgb(0, 0, 0)'
+    }
+    else {
+        document.body.style.backgroundColor = 'rgb(30, 35, 55)'
+        document.getElementById('themeIcon').className = 'far fa-sun'
+        document.getElementById('themeIcon').style.color = 'rgb(255, 255, 255)'
+        document.getElementById('tonLogo').src = 'assets/img/ton_logo_dark_background.svg'
+        document.getElementById('mainBannerText').style.color = 'rgb(255, 255, 255)'
+        document.getElementById('statsBanner').style.color = 'rgb(255, 255, 255)'
+    }
+}
+
+const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
+    manifestUrl: 'https://shp3x.github.io/tonconnect-manifest.json',
+    buttonRootId: 'ton-connect',
+});
+
+function enableExchangeButton() {
+    document.getElementById('joinButton').disabled = false;
+}
+
+async function showModalWindow() {
+    console.log("Wallet connection state", tonConnectUI.connected)
+
+    if (document.body.style.backgroundColor == "rgb(30, 35, 55)") {
+        document.getElementById('modalContent').style.backgroundColor = 'rgb(30, 35, 55)'
+        document.getElementById('modalTitle').style.color = 'rgb(247, 249, 251)'
+    } else {
+        document.getElementById('modalContent').style.backgroundColor = 'rgb(247, 249, 251)'
+        document.getElementById('modalTitle').style.color = 'rgb(30, 35, 55)'
+    }
+
+    if (tonConnectUI.connected == true) {
+        $('#walletAlert').addClass('d-none')
+        $('#amountValue').prop("readonly", false)
+        $('#cardNumber').prop("readonly", false)
+        document.getElementById('exchangeButton').disabled = false;
+
+        $('#myModal').modal('show');
+    }
+    else {
+        $('#walletAlert').removeClass('d-none')
+        $('#amountValue').prop("readonly", true)
+        $('#cardNumber').prop("readonly", true)
+        document.getElementById('exchangeButton').disabled = true;
+
+        $('#myModal').modal('show');
+    }
+}
+
+async function changeAmount() {
+    amount = document.getElementById('amountValue').value
+    document.getElementById('receiveValue').value = parseFloat(amount * 6.6).toFixed(2);
+}
+
+async function createTransaction() {
+    const transaction = {
+    validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+    messages: [
+        {
+            address: "UQDjF5iuLIovsxqY7L0KPAek7OdZn3m6mZJhHlJOiP2qTo8O",
+            amount: $('#amountValue').val() * 1000000000
+        }
+    ]
+    }
+    try {
+    const result = await tonConnectUI.sendTransaction(transaction,{
+    modals: ['before', 'success', 'error'],
+    notifications: ['before', 'success', 'error']
+});
+    const someTxData = await myAppExplorerService.getTransaction(result.boc);
+    alert('Transaction was sent successfully', someTxData);
+} catch (e) {
+        console.error(e); 
+    }
+}
